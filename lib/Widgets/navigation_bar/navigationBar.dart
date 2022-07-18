04b71f256fe901e632d9aabe4fa8463d9245790e
5thfader <chrisjeremy84@live.com>
2022-07-18 18:18:25 +0200

    Adding responsiveness

--- lib/Widgets/navigation_bar/navigationBar.dart
+++ lib/Widgets/navigation_bar/navigationBar.dart
@@ -8,24 +8,6 @@ class NavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        const Padding(padding: EdgeInsets.all(10), child: NavBarLogo()),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: const [
-            NavBarItem(title: "Home"),
-            SizedBox(width: 60),
-            NavBarItem(title: "Products & Services"),
-            SizedBox(width: 60),
-            NavBarItem(title: "Pricing"),
-            SizedBox(width: 60),
-            NavBarItem(title: "Login"),
-            SizedBox(width: 60),
-          ],
-        )
-      ],
-    );
+    return 
   }
 }
