4869c5f81b2c53a30a93f6ba4a86df493f1cab00
5thfader <chrisjeremy84@live.com>
2022-08-05 00:14:31 -0400

    Cards... not rendering correctly

--- lib/Screens/Product_Services/Product_Services.dart
+++ lib/Screens/Product_Services/Product_Services.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
-import '../../Widgets/product_services_cards/cards.dart';
-import 'Product_Services_Desktop.dart';
-import 'Product_Services_Mobile.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
+/*
+
+05-08-2022
+----------
+ Cards design and grid layout failed...
+      Not rendering the way it's supposed to.
+
+
+      Need a retry with different grid techniques...
+      the main issue is the cards are not the actual size layed out on the map.  
+
+
+
+
+ */
 class Product_Services extends StatelessWidget {
   const Product_Services({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ScreenTypeLayout(
-        mobile: const ProductServicesMobile(),
-        desktop: const ProductServicesDesktop());
+    return Container();
   }
 }
